589036fe564892ae07959980d8c6b40883c5d99a
guyslab <guy@smartagps.com>
2021-02-04 12:59:28 +0200

    mongo id fix

--- ResponseConsumer/Entities/Response.cs
+++ ResponseConsumer/Entities/Response.cs
@@ -12,10 +12,17 @@ namespace ResponseConsumer.Entities
 
         public string Url { get; set; }
         public byte Priority { get; set; }
+        
+        [BsonRepresentation(BsonType.String)]
         public DateTimeOffset TimestampUtc { get; set; }
         public short StatusCode { get; set; }
         public string Base64Body { get; set; }
 
         public string CorrelationId { get; set; }
+
+        public Response()
+        {
+            Id = ObjectId.GenerateNewId().ToString();
+        }
     }
 }
